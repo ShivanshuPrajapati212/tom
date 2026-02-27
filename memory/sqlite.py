ab818d7d3736f4b484fe0197e3dd76070b4e1ddd
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-02-27 10:56:26 +0530

    hey, sqlite is here

---
+++ memory/sqlite.py
@@ -0,0 +1,28 @@
+from datetime import datetime
+import sqlite3
+
+
+conn = sqlite3.connect("long_term_memory.db")
+cursor = conn.cursor()
+
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS events (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        event_type TEXT NOT NULL,
+        summary TEXT NOT NULL,
+        importance REAL,
+        created_at TEXT
+    )
+""")
+conn.commit()
+
+def add_to_db(event_type, summary, importance):
+    now = datetime.now().isoformat()  # "2026-02-26T14:30:00.123456"
+    cursor.execute("INSERT INTO events (event_type, summary, importance, created_at) VALUES (?, ?, ?, ?)", (event_type, summary, importance, now))
+    conn.commit()
+
+
+if __name__ == "__main__":
+    cursor.execute("SELECT * FROM events")
+    rows = cursor.fetchall()
+    print(rows)
---
+++ memory/update_memory.py
@@ -0,0 +1,6 @@
+
+
+def update_memory(inputs):
+    for input in inputs:
+        if input.input_type.startswith("file"):
+                    
