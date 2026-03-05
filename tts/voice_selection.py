671ccbaa657f059dc9ad484a531f8b7e00e7d2ae
Shivanshu Prajapati <shivanshuprajapati212@gmail.com>
2026-03-05 07:44:22 +0530

    tts init

---
+++ tts/voice_selection.py
@@ -0,0 +1,12 @@
+from AppKit import NSSpeechSynthesizer
+from Foundation import NSRunLoop, NSDate
+
+speech = NSSpeechSynthesizer.alloc().init()
+
+speech.startSpeakingString_("Hi.")
+
+# Keep the run loop alive while speaking
+while speech.isSpeaking():
+    NSRunLoop.currentRunLoop().runUntilDate_(
+        NSDate.dateWithTimeIntervalSinceNow_(0.1)
+    )
